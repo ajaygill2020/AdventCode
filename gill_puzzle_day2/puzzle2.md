153df5e2a41ac88a8fe2206f2115832d9f953df0
ajaygill2020 <ajaypalsgill@gmail.com>
2023-05-31 22:07:16 -0400

    Add puzzles for day2 and day3 projects

---
+++ gill_puzzle_day2/puzzle2.md
@@ -0,0 +1,15 @@
+# Programming Puzzle #2
+
+Now that you know how AoC works. Let's keep going.
+
+Now you'll solve the Day 2 puzzles (part 1 and part 2) from [AoC 2021](https://adventofcode.com/2021). This problem is pretty straight forward. Just read the instructions carefully - especially in Part 2.
+
+For our class purposes, you need to do a few things:
+
+1. You need to write the Python code to solve the puzzles. Your code should be **well commented** and convey your basic strategy for how you are solving the problem. In particular, discuss why you chose to use the data structures you did. 
+
+You can do this in a Jupyter notebook if you'd like or use an IDE such as PyCharm or VSCode. 
+
+2. After solving (or not) the puzzle, you will submit your Jupyter notebook or .py file via Moodle in this Assignment upload area.
+
+3. After you solve the Day 2 puzzles, submit your answer in your AoC account and then take a screenshot of your AoC main page so I can see your gold stars. Upload your screenshot as a png or jpg file along with your notebook or .py file.
---
+++ gill_puzzle_day_three/gill_puzzle_day3.py
@@ -0,0 +1,91 @@
+# Ajay Gill
+# Advent of Code 2021 : Puzzle 3 Parts 1 and 2
+# 31 May 2023
+
+# Goal = What is the power consumption of the submarine? (Be sure to represent your answer in decimal, not binary.)
+
+test = True
+if test:
+    data_file = 'test_input.txt'
+else:
+    data_file = 'data/input.txt'
+
+# Assign the input data into the variable known as 'bits'
+with open(data_file, 'r') as file_input:
+    report_numbers = file_input.read().splitlines()
+
+# Print out how many bits there are in the input file
+total_commands = len(report_numbers)
+print(f'There are {total_commands} total binary numbers')
+
+# # Declare and assign positions to zero
+# horizontal_position = 0
+# depth = 0
+
+# # Condition that read the array (sumbarine_commands) and re-assign positions
+# for i in range(0, total_commands):
+#     # Forward command
+#     if (array_commands[i].startswith('f')):
+#         last_num = array_commands[i][-1]
+#         horizontal_position += int(last_num)
+#     # Up command
+#     if (array_commands[i].startswith('u')):
+#         last_num = array_commands[i][-1]
+#         depth -= int(last_num)
+#     # Down command
+#     if (array_commands[i].startswith('d')):
+#         last_num = array_commands[i][-1]
+#         depth += int(last_num)
+
+# # Print out the horizontal and depth positions since they were re-assigned in the prior if blocks
+# print(f"Your horizontal position is : {horizontal_position}  and depth is : {depth}") 
+# answer = horizontal_position * depth
+# print(f"Answer to the part 1 puzzle is : {answer}\n\n\n\n")
+# # Answer is horizontal = 1950 and depth = 823. The product of these two numbers = 1,604,850
+
+# # ******************************************************************************
+# print()
+# # Below is my Day 2 Puzzle Part 2
+# # Goal: What do you get if you multiply your final horizontal position by your final depth?
+
+# # Define if we are using the test file or non-test input file
+# test = False
+# if test:
+#     data_file = 'test_input.txt'
+# else:
+#     data_file = 'data/input_part2.txt'
+
+# # Assign the input data into the variable known as 'array_commands'
+# with open(data_file, 'r') as file_input:
+#     array_commands = file_input.read().splitlines()
+
+# # Print out how many commands there are in the input file
+# total_commands = len(array_commands)
+# print(f'There are {total_commands} total submarine command(s)')
+
+# # Declare and assign positions to zero
+# horizontal_position = 0
+# depth = 0
+# aim = 0
+
+# # Condition that read the array (sumbarine_commands) and re-assign positions
+# for i in range(0, total_commands):
+#     # Forward command
+#     if (array_commands[i].startswith('f')):
+#         last_num = array_commands[i][-1]
+#         horizontal_position += int(last_num)
+#         depth += (int(last_num) * aim)
+#     # Up command
+#     if (array_commands[i].startswith('u')):
+#         last_num = array_commands[i][-1]
+#         aim -= int(last_num)
+#     # Down command
+#     if (array_commands[i].startswith('d')):
+#         last_num = array_commands[i][-1]
+#         aim += int(last_num)
+
+# # Print out the horizontal and depth positions since they were re-assigned in the prior if blocks
+# print(f"Your horizontal position is : {horizontal_position}  ,  depth is : {depth}  ,  and aim is : {aim}") 
+# answer = horizontal_position * depth
+# print(f"Answer to the puzzle is : {answer}")
+# # Answer is horizontal = 1950, depth = 864198, and aim = 823. The answer = 1,685,186,100
